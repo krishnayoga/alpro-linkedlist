142d8ade21f92356cd2086aaf9b20a7a2d2fac39
Fadel <33676516+fadelerian@users.noreply.github.com>
2017-11-19 17:50:52 +0700

    Update delete.h

--- delete.h
+++ delete.h
@@ -1,15 +1,33 @@
-void delete(struct node** head_ref)
+/* Memberikan referensi simpul pada awal linked list
+dan memberikan isi simpul pertama yang ditemukan
+ yang ingin dihilangkan (key) */
+void delete(struct node **head_ref, int key)
 {
-
-   struct node* current = *head_ref;
-   struct node* next;
+    // Store head node
+    struct node* temp = *head_ref, *prev;
  
-   while (current != NULL) 
-   {
-       next = current->next;
-       free(current);
-       current = next;
-   }
-   
-   *head_ref = NULL;
+    // If head node itself holds the key to be deleted
+    if (temp != NULL && temp->data == key)
+    {
+        *head_ref = temp->next;   // Changed head
+        free(temp);               // free old head
+        return;
+    }
+ 
+    /*Mencari data yang ingin dihapuskan
+    Sembari mencari simpul yang berisikan data, 
+	memberi jejak pada tiap simpul yang akan dipindahkan referensinya*/
+    while (temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // Apabila yang ingin dihapus tidak ada
+    if (temp == NULL) return;
+ 
+    // Memindahkan referensi simpul menuju ke simpul berikutnya
+    prev->next = temp->next;
+ 
+    free(temp);  // Melepas memori
 }
